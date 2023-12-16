6bd0fe8b656cbc0b54d8a2652a4e2ff67b817140
yama-pixed <146500402+yama-pixed@users.noreply.github.com>
2023-12-16 12:27:29 -0500

    Testing

---
+++ if.py
@@ -0,0 +1,12 @@
+print("Enter your name: ")
+name=str(input())
+
+print("Enter your age: ")
+
+age =int(input())
+ 
+if age < 18:
+    print("sorry")
+else:
+    print("u can apply")
+
---
+++ number.py
@@ -0,0 +1,12 @@
+
+
+a = 10
+b = 10
+
+
+if a < 20 or b < 30:
+	print("Either of the number is greater than 0") 
+else: 
+	print("No number is greater than 0") 
+
+
